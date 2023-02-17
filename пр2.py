1f3fd2fdbbebfb1b913e930fba344a2e003b857c
nikiwwwwwww <102387493+nikiwwwwwww@users.noreply.github.com>
2023-02-17 12:32:05 +0300

    Update пр2.py

--- пр2.py
+++ пр2.py
@@ -1,15 +1,18 @@
-mesycaVes = [31,28,31,30,31,30,31,31,30,31,30,31]
-mesycaDef = [31,29,31,30,31,30,31,31,30,31,30,31]
-mesyc = []
+mesyca = [31,28,31,30,31,30,31,31,30,31,30,31]
 print("Введите год")
 result = 0
 god = int(input())
-if((god % 4) == 0):
-    mesyc = mesycaVes
+if god % 4 != 0:
+    mesyca[1] = 28
+elif god % 100 == 0:
+    if god % 400 == 0:
+        mesyca[1] = 29
+    else:
+        mesyca[1] = 28
 else:
-    mesyc = mesycaDef
+    mesyca[1] = 29
 for i in range(0,12):
-    for j in range(1,mesyc[i]+1):
+    for j in range(1,mesyca[i]+1):
         if(j>9):
             while j > 0:
                 result = result + (j % 10)
